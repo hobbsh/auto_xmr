ee73e01511ebc082b424a38d653a4bbc0d7d1582
Wylie Hobbs <wylie@wyliehobbs.com>
2018-01-15 14:46:33 -0700

    Add stop on error flag

--- mine.ps1
+++ mine.ps1
@@ -1,4 +1,5 @@
-﻿
+﻿$ErrorActionPreference = "Stop"
+
 C:\auto_monero\scripts\devcon.ps1
 C:\auto_monero\overdriventool\OverdriveNTool.exe -p0"vega_fe" -p1"vega64" -p2"vega_fe" -p3"vega_fe"
 C:\auto_monero\miner\mine.bat
