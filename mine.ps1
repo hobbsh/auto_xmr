905ff990e1c146164cbc44a33c7f12c659940377
Wylie Hobbs <wyie@wyliehobbs.com>
2018-01-31 17:34:21 -0700

    Add self-elevate for mine.ps1 and update overdriventool exec to account for igpu

--- mine.ps1
+++ mine.ps1
@@ -1,7 +1,40 @@
 ﻿$ErrorActionPreference = "Stop"
 
+# Get the ID and security principal of the current user account
+$myWindowsID=[System.Security.Principal.WindowsIdentity]::GetCurrent()
+$myWindowsPrincipal=new-object System.Security.Principal.WindowsPrincipal($myWindowsID)
+ 
+# Get the security principal for the Administrator role
+$adminRole=[System.Security.Principal.WindowsBuiltInRole]::Administrator
+ 
+# Check to see if we are currently running "as Administrator"
+if ($myWindowsPrincipal.IsInRole($adminRole)){
+   # We are running "as Administrator" - so change the title and background color to indicate this
+   $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)"
+   $Host.UI.RawUI.BackgroundColor = "DarkBlue"
+   clear-host
+}
+else {
+   # We are not running "as Administrator" - so relaunch as administrator
+   
+   # Create a new process object that starts PowerShell
+   $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+   
+   # Specify the current script path and name as a parameter
+   $newProcess.Arguments = $myInvocation.MyCommand.Definition;
+   
+   # Indicate that the process should be elevated
+   $newProcess.Verb = "runas";
+   
+   # Start the new process
+   [System.Diagnostics.Process]::Start($newProcess);
+   
+   # Exit from the current, unelevated, process
+   exit
+}
+ 
 C:\auto_monero\scripts\devcon.ps1
-C:\auto_monero\overdriventool\OverdriveNTool.exe -p0"vega_fe" -p1"vega_fe" -p2"vega_fe" -p3"vega_fe" -p4"vega_fe"
+C:\auto_monero\overdriventool\OverdriveNTool.exe -p1"vega_fe" -p2"vega64" -p3"vega_fe" -p4"vega_fe" -p5"vega_fe"
 
 set GPU_FORCE_64BIT_PTR=1
 set GPU_MAX_HEAP_SIZE=99
